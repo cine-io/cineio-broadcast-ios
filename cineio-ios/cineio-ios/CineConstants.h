5231b76290ae301ca346afdff7473495e2cc4c3d
Jeffrey Wescott <jeffrey.wescott@gmail.com>
2014-08-19 17:22:41 -0700

    Removed unused files. Upgrade to latest VideoCore. Version bump.

--- cineio-ios/cineio-ios/CineConstants.h
+++ cineio-ios/cineio-ios/CineConstants.h
@@ -1,6 +1,6 @@
 #define API_VERSION "1"
 #define BASE_URL "https://www.cine.io/api/"
-#define SDK_VERSION "0.4.2"
+#define SDK_VERSION "0.4.3"
 #define USER_AGENT "cineio-ios"
 
 extern NSString* const BaseUrl;
