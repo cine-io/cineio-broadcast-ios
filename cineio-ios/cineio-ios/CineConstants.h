1e8739c64c0ba6948297338502fbe8e33388958a
Jeffrey Wescott <jeffrey.wescott@gmail.com>
2014-09-08 17:49:28 -0700

    Update version.

--- cineio-ios/cineio-ios/CineConstants.h
+++ cineio-ios/cineio-ios/CineConstants.h
@@ -1,6 +1,6 @@
 #define API_VERSION "1"
 #define BASE_URL "https://www.cine.io/api/"
-#define SDK_VERSION "0.4.4"
+#define SDK_VERSION "0.4.5"
 #define USER_AGENT "cineio-ios"
 
 extern NSString* const BaseUrl;
