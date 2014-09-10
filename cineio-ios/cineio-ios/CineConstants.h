c56e8701ececbf216987c5b589ba9ebbf03a1674
Jeffrey Wescott <jeffrey.wescott@gmail.com>
2014-09-09 22:37:47 -0700

    Upgrade to latest VideoCore.

--- cineio-ios/cineio-ios/CineConstants.h
+++ cineio-ios/cineio-ios/CineConstants.h
@@ -1,6 +1,6 @@
 #define API_VERSION "1"
 #define BASE_URL "https://www.cine.io/api/"
-#define SDK_VERSION "0.4.6"
+#define SDK_VERSION "0.4.7"
 #define USER_AGENT "cineio-ios"
 
 extern NSString* const BaseUrl;
