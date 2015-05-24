410a3db5cc80596d8327b0e234d8093d737bae58
Jeffrey Wescott <jeffrey.wescott@gmail.com>
2015-05-24 10:48:18 -0700

    version bump (this time, including the version constant)

--- cineio-broadcast-ios/cineio-broadcast-ios/CineConstants.h
+++ cineio-broadcast-ios/cineio-broadcast-ios/CineConstants.h
@@ -2,7 +2,7 @@
 
 #define API_VERSION "1"
 #define BASE_URL "https://www.cine.io/api/"
-#define SDK_VERSION "0.6.2"
+#define SDK_VERSION "0.6.4"
 #define USER_AGENT "cineio-ios"
 
 extern NSString* const BaseUrl;
