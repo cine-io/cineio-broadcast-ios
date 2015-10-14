9461623cdb548fc0ad0ae6d6d94e33c3a6a1e324
施灵凯 <manshilingkai@gmail.com>
2015-10-14 19:25:32 +0800

    add ijkplayer function

--- arenacloud-broadcast-ios/arenacloud-broadcast-ios/ACBRPlayerViewController.h
+++ arenacloud-broadcast-ios/arenacloud-broadcast-ios/ACBRPlayerViewController.h
@@ -7,14 +7,18 @@
 //
 
 #import <UIKit/UIKit.h>
-#import <MediaPlayer/MediaPlayer.h>
+//#import <MediaPlayer/MediaPlayer.h>
 #import "ACBRStream.h"
+
+#import "IJKMediaPlayback.h"
 
 @interface ACBRPlayerViewController : UIViewController
 
 @property (nonatomic, strong) UIActivityIndicatorView *spinner;
 @property (nonatomic, strong) ACBRStream *stream;
-@property (nonatomic, strong) MPMoviePlayerController *moviePlayer;
+
+//@property (nonatomic, strong) MPMoviePlayerController *moviePlayer;
+@property (nonatomic, strong) id<IJKMediaPlayback> player;
 
 - (void)startStreaming;
 - (void)finishStreaming;
--- arenacloud-broadcast-ios/arenacloud-broadcast-ios/ACBRPlayerViewController.m
+++ arenacloud-broadcast-ios/arenacloud-broadcast-ios/ACBRPlayerViewController.m
@@ -9,15 +9,20 @@
 #import "ACBRPlayerViewController.h"
 #import <AFNetworking/AFNetworking.h>
 
+#import "IJKMediaPlayer.h"
+
 @interface ACBRPlayerViewController ()
 
 @end
 
 @implementation ACBRPlayerViewController
+{
+    NSMutableArray *_registeredNotifications;
+}
 
 @synthesize spinner;
 @synthesize stream;
-@synthesize moviePlayer;
+//@synthesize moviePlayer;
 
 - (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
 {
@@ -60,9 +65,11 @@
 {
     NSAssert(stream != nil, @"The stream was never set for this player.");
     [spinner startAnimating];
-    [self attemptToPlayStream:0];
+//    [self attemptToPlayStream:0];
+    [self playStream];
 }
 
+/*
 - (void)playStream
 {
     NSLog(@"about to play: %@", stream.playUrlHLS);
@@ -88,7 +95,274 @@
     [moviePlayer setScalingMode:MPMovieScalingModeAspectFill];
     [moviePlayer setFullscreen:YES animated:YES];
 }
+*/
 
+- (void)playStream
+{
+    [[UIApplication sharedApplication] setStatusBarHidden:YES];
+    [[UIApplication sharedApplication] setStatusBarOrientation:UIInterfaceOrientationLandscapeLeft animated:NO];
+    
+    _registeredNotifications = [[NSMutableArray alloc] init];
+    [self registerApplicationObservers];
+    
+    _registeredNotifications = [[NSMutableArray alloc] init];
+    [self registerApplicationObservers];
+    
+    //for test
+//    NSURL *theMovieURL = [NSURL URLWithString:@"http://v.iask.com/v_play_ipad.php?vid=99264895"];
+    NSURL *theMovieURL = [NSURL URLWithString:stream.playUrlHLS];
+    
+    [IJKFFMoviePlayerController setLogReport:YES];
+    IJKFFOptions *options = [IJKFFOptions optionsByDefault];
+    self.player = [[IJKFFMoviePlayerController alloc] initWithContentURL:theMovieURL withOptions:options];
+    
+    self.player.view.autoresizingMask = UIViewAutoresizingFlexibleWidth|UIViewAutoresizingFlexibleHeight;
+    self.player.view.frame = self.view.bounds;
+    
+    self.view.autoresizesSubviews = YES;
+    [self.view addSubview:self.player.view];
+    
+    [self installMovieNotificationObservers];
+    
+    [self.player prepareToPlay];
+}
+
+- (void)loadStateDidChange:(NSNotification*)notification
+{
+    //    MPMovieLoadStateUnknown        = 0,
+    //    MPMovieLoadStatePlayable       = 1 << 0,
+    //    MPMovieLoadStatePlaythroughOK  = 1 << 1, // Playback will be automatically started in this state when shouldAutoplay is YES
+    //    MPMovieLoadStateStalled        = 1 << 2, // Playback will be automatically paused in this state, if started
+    
+    MPMovieLoadState loadState = _player.loadState;
+    
+    if ((loadState & MPMovieLoadStatePlaythroughOK) != 0) {
+        NSLog(@"loadStateDidChange: MPMovieLoadStatePlaythroughOK: %d\n", (int)loadState);
+    } else if ((loadState & MPMovieLoadStateStalled) != 0) {
+        NSLog(@"loadStateDidChange: MPMovieLoadStateStalled: %d\n", (int)loadState);
+    } else {
+        NSLog(@"loadStateDidChange: ???: %d\n", (int)loadState);
+    }
+}
+
+- (void)moviePlayBackDidFinish:(NSNotification*)notification
+{
+    //    MPMovieFinishReasonPlaybackEnded,
+    //    MPMovieFinishReasonPlaybackError,
+    //    MPMovieFinishReasonUserExited
+    int reason = [[[notification userInfo] valueForKey:MPMoviePlayerPlaybackDidFinishReasonUserInfoKey] intValue];
+    
+    switch (reason)
+    {
+        case MPMovieFinishReasonPlaybackEnded:
+            NSLog(@"playbackStateDidChange: MPMovieFinishReasonPlaybackEnded: %d\n", reason);
+            break;
+            
+        case MPMovieFinishReasonUserExited:
+            NSLog(@"playbackStateDidChange: MPMovieFinishReasonUserExited: %d\n", reason);
+            break;
+            
+        case MPMovieFinishReasonPlaybackError:
+            NSLog(@"playbackStateDidChange: MPMovieFinishReasonPlaybackError: %d\n", reason);
+            break;
+            
+        default:
+            NSLog(@"playbackPlayBackDidFinish: ???: %d\n", reason);
+            break;
+    }
+    
+    [self removeMovieNotificationObservers];
+    [self.player shutdown];
+    self.player = nil;
+}
+
+- (void)mediaIsPreparedToPlayDidChange:(NSNotification*)notification
+{
+    NSLog(@"mediaIsPreparedToPlayDidChange\n");
+    [self.player play];
+}
+
+- (void)moviePlayBackStateDidChange:(NSNotification*)notification
+{
+    //    MPMoviePlaybackStateStopped,
+    //    MPMoviePlaybackStatePlaying,
+    //    MPMoviePlaybackStatePaused,
+    //    MPMoviePlaybackStateInterrupted,
+    //    MPMoviePlaybackStateSeekingForward,
+    //    MPMoviePlaybackStateSeekingBackward
+    
+    switch (_player.playbackState)
+    {
+        case MPMoviePlaybackStateStopped: {
+            NSLog(@"moviePlayBackStateDidChange %d: stoped", (int)_player.playbackState);
+            break;
+        }
+        case MPMoviePlaybackStatePlaying: {
+            NSLog(@"moviePlayBackStateDidChange %d: playing", (int)_player.playbackState);
+            break;
+        }
+        case MPMoviePlaybackStatePaused: {
+            NSLog(@"moviePlayBackStateDidChange %d: paused", (int)_player.playbackState);
+            break;
+        }
+        case MPMoviePlaybackStateInterrupted: {
+            NSLog(@"moviePlayBackStateDidChange %d: interrupted", (int)_player.playbackState);
+            break;
+        }
+        case MPMoviePlaybackStateSeekingForward:
+        case MPMoviePlaybackStateSeekingBackward: {
+            NSLog(@"moviePlayBackStateDidChange %d: seeking", (int)_player.playbackState);
+            break;
+        }
+        default: {
+            NSLog(@"moviePlayBackStateDidChange %d: unknown", (int)_player.playbackState);
+            break;
+        }
+    }
+}
+
+
+/* Register observers for the various movie object notifications. */
+-(void)installMovieNotificationObservers
+{
+    [[NSNotificationCenter defaultCenter] addObserver:self
+                                             selector:@selector(loadStateDidChange:)
+                                                 name:IJKMoviePlayerLoadStateDidChangeNotification
+                                               object:_player];
+    
+    [[NSNotificationCenter defaultCenter] addObserver:self
+                                             selector:@selector(moviePlayBackDidFinish:)
+                                                 name:IJKMoviePlayerPlaybackDidFinishNotification
+                                               object:_player];
+    
+    [[NSNotificationCenter defaultCenter] addObserver:self
+                                             selector:@selector(mediaIsPreparedToPlayDidChange:)
+                                                 name:IJKMediaPlaybackIsPreparedToPlayDidChangeNotification
+                                               object:_player];
+    
+    [[NSNotificationCenter defaultCenter] addObserver:self
+                                             selector:@selector(moviePlayBackStateDidChange:)
+                                                 name:IJKMoviePlayerPlaybackStateDidChangeNotification
+                                               object:_player];
+}
+
+/* Remove the movie notification observers from the movie object. */
+-(void)removeMovieNotificationObservers
+{
+    [[NSNotificationCenter defaultCenter]removeObserver:self name:IJKMoviePlayerLoadStateDidChangeNotification object:_player];
+    [[NSNotificationCenter defaultCenter]removeObserver:self name:IJKMoviePlayerPlaybackDidFinishNotification object:_player];
+    [[NSNotificationCenter defaultCenter]removeObserver:self name:IJKMediaPlaybackIsPreparedToPlayDidChangeNotification object:_player];
+    [[NSNotificationCenter defaultCenter]removeObserver:self name:IJKMoviePlayerPlaybackStateDidChangeNotification object:_player];
+}
+
+- (void)applicationWillEnterForeground
+{
+    NSLog(@"applicationWillEnterForeground: %d", (int)[UIApplication sharedApplication].applicationState);
+    dispatch_async(dispatch_get_main_queue(), ^{
+        
+        //for test
+        //NSURL *theMovieURL = [NSURL URLWithString:@"http://v.iask.com/v_play_ipad.php?vid=99264895"];
+        NSURL *theMovieURL = [NSURL URLWithString:stream.playUrlHLS];
+        
+        [IJKFFMoviePlayerController setLogReport:YES];
+        IJKFFOptions *options = [IJKFFOptions optionsByDefault];
+        self.player = [[IJKFFMoviePlayerController alloc] initWithContentURL:theMovieURL withOptions:options];
+        self.player.view.autoresizingMask = UIViewAutoresizingFlexibleWidth|UIViewAutoresizingFlexibleHeight;
+        self.player.view.frame = self.view.bounds;
+        
+        self.view.autoresizesSubviews = YES;
+        [self.view addSubview:self.player.view];
+        
+        [self installMovieNotificationObservers];
+        
+        [self.player prepareToPlay];
+        
+    });
+}
+
+- (void)applicationDidBecomeActive
+{
+    NSLog(@"applicationDidBecomeActive: %d", (int)[UIApplication sharedApplication].applicationState);
+    dispatch_async(dispatch_get_main_queue(), ^{
+    });
+    
+}
+
+- (void)applicationWillResignActive
+{
+    NSLog(@"applicationWillResignActive: %d", (int)[UIApplication sharedApplication].applicationState);
+    dispatch_async(dispatch_get_main_queue(), ^{
+        [self.player pause];
+    });
+}
+
+- (void)applicationDidEnterBackground
+{
+    NSLog(@"applicationDidEnterBackground: %d", (int)[UIApplication sharedApplication].applicationState);
+    dispatch_async(dispatch_get_main_queue(), ^{
+        [self removeMovieNotificationObservers];
+        [self.player shutdown];
+        self.player = nil;
+    });
+}
+
+- (void)applicationWillTerminate
+{
+    NSLog(@"applicationWillTerminate: %d", (int)[UIApplication sharedApplication].applicationState);
+    dispatch_async(dispatch_get_main_queue(), ^{
+        [self removeMovieNotificationObservers];
+        [self.player shutdown];
+        self.player = nil;
+        
+    });
+}
+
+
+- (void)registerApplicationObservers
+{
+    
+    [[NSNotificationCenter defaultCenter] addObserver:self
+                                             selector:@selector(applicationWillEnterForeground)
+                                                 name:UIApplicationWillEnterForegroundNotification
+                                               object:nil];
+    [_registeredNotifications addObject:UIApplicationWillEnterForegroundNotification];
+    
+    [[NSNotificationCenter defaultCenter] addObserver:self
+                                             selector:@selector(applicationDidBecomeActive)
+                                                 name:UIApplicationDidBecomeActiveNotification
+                                               object:nil];
+    [_registeredNotifications addObject:UIApplicationDidBecomeActiveNotification];
+    
+    [[NSNotificationCenter defaultCenter] addObserver:self
+                                             selector:@selector(applicationWillResignActive)
+                                                 name:UIApplicationWillResignActiveNotification
+                                               object:nil];
+    [_registeredNotifications addObject:UIApplicationWillResignActiveNotification];
+    
+    [[NSNotificationCenter defaultCenter] addObserver:self
+                                             selector:@selector(applicationDidEnterBackground)
+                                                 name:UIApplicationDidEnterBackgroundNotification
+                                               object:nil];
+    [_registeredNotifications addObject:UIApplicationDidEnterBackgroundNotification];
+    
+    [[NSNotificationCenter defaultCenter] addObserver:self
+                                             selector:@selector(applicationWillTerminate)
+                                                 name:UIApplicationWillTerminateNotification
+                                               object:nil];
+    [_registeredNotifications addObject:UIApplicationWillTerminateNotification];
+}
+
+- (void)unregisterApplicationObservers
+{
+    for (NSString *name in _registeredNotifications) {
+        [[NSNotificationCenter defaultCenter] removeObserver:self
+                                                        name:name
+                                                      object:nil];
+    }
+}
+
+
+/*
 - (void)attemptToPlayStream:(NSUInteger)numTries
 {
     if (numTries < 3) {
@@ -119,6 +393,7 @@
     }
 }
 
+
 - (void)stoppedStreaming:(NSNotification*)notification {
     NSLog(@"stopped streaming");
     MPMoviePlayerController *player = [notification object];
@@ -137,10 +412,16 @@
     }
     [self finishStreaming];
 }
+*/
 
 - (void)finishStreaming
 {
     // subclasses should implement this method
+    [self removeMovieNotificationObservers];
+    [self.player shutdown];
+    self.player = nil;
+    
+    [self unregisterApplicationObservers];
 }
 
 
